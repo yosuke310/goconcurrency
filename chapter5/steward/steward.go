99d6c2ce04257de80ae6b96dc3e5f09f4ca53409
yosuke310 <yosuke310@gmail.com>
2019-03-12 23:49:59 +0900

    Chapter5

---
+++ chapter5/steward/steward.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type startGoroutineFn func(
+	done <-chan interface{},
+	pulseInterval time.Duration,
+) (heartbeat <-chan interface{})
+
+func newSteward(timeout time.Duration,
+	startGoroutine startGoroutineFn) startGoroutineFn {
+	return func(done <-chan interface{}, pulseInterval time.Duration) <-chan interface{} {
+		heartbeat := make(chan interface{})
+		go func() {
+			defer close(heartbeat)
+
+			var wardDone chan interface{}
+			var wardHeartbeat <-chan interface{}
+			startWard := func() {
+				wardDone = make(chan interface{})
+				wardHeartbeat = startGoroutine(or(wardDone, done), timeout/2)
+			}
+			startWard()
+			pulse := time.Tick(pulseInterval)
+
+		monitorLoop:
+			for {
+				timeoutSignal := time.After(timeout)
+				for {
+					select {
+					case <-pulse:
+						select {
+						case heartbeat <- struct{}{}:
+						default:
+						}
+					case <-wardHeartbeat:
+						continue monitorLoop
+					case <-timeoutSignal:
+						log.Println("steward: eard unhealty; restarting")
+						close(wardDone)
+						startWard()
+						continue monitorLoop
+					case <-done:
+						return
+					}
+				}
+			}
+		}()
+		return heartbeat
+	}
+}
+
+func doWorkFn(done <-chan interface{}, intList ...int) (startGoroutineFn, <-chan interface{}) {
+	intChanStream := make(chan (<-chan interface{}))
+	intStream := bridge(done, intChanStream)
+
+	doWork := func(done <-chan interface{}, pulseInterval time.Duration) <-chan interface{} {
+		intStream := make(chan interface{})
+		heartbeat := make(chan interface{})
+		go func() {
+			defer close(intStream)
+			select {
+			case intChanStream <- intStream:
+			case <-done:
+				return
+			}
+			pulse := time.Tick(pulseInterval)
+
+			for {
+			valueLoop:
+				for _, intVal := range intList {
+					if intVal < 0 {
+						log.Printf("negative value: %v\n", intVal)
+						return
+					}
+					for {
+						select {
+						case <-pulse:
+							select {
+							case heartbeat <- struct{}{}:
+							default:
+							}
+						case intStream <- intVal:
+							continue valueLoop
+						case <-done:
+							return
+						}
+					}
+				}
+			}
+		}()
+		return heartbeat
+	}
+
+	return doWork, intStream
+}
+
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+
+	var done chan interface{}
+	done = make(chan interface{})
+	go func() {
+		defer close(done)
+		switch len(channels) {
+		case 2:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			case <-channels[2]:
+			case <-or(append(channels[3:], done)...):
+			}
+		}
+	}()
+	return done
+}
+
+func bridge(done <-chan interface{}, chanStream <-chan <-chan interface{}) <-chan interface{} {
+	valStream := make(chan interface{})
+	go func() {
+		defer close(valStream)
+		for {
+			var stream <-chan interface{}
+			select {
+			case maybeStream, ok := <-chanStream:
+				if ok == false {
+					return
+				}
+				stream = maybeStream
+			case <-done:
+				return
+			}
+			for val := range orDone(done, stream) {
+				select {
+				case valStream <- val:
+				case <-done:
+				}
+			}
+		}
+	}()
+	return valStream
+}
+
+func orDone(done, c <-chan interface{}) <-chan interface{} {
+	valStream := make(chan interface{})
+	go func() {
+		defer close(valStream)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-c:
+				if ok == false {
+					return
+				}
+				select {
+				case valStream <- v:
+				case <-done:
+				}
+			}
+		}
+	}()
+	return valStream
+}
+
+func take(done <-chan interface{}, valueStream <-chan interface{}, num int) <-chan interface{} {
+	takeStream := make(chan interface{})
+	go func() {
+		defer close(takeStream)
+		for i := 0; i < num; i++ {
+			select {
+			case <-done:
+				return
+			case takeStream <- <-valueStream:
+			}
+		}
+	}()
+	return takeStream
+}
+
+func main() {
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime | log.LUTC)
+
+	done := make(chan interface{})
+	defer close(done)
+
+	doWork, intStream := doWorkFn(done, 1, 2, -1, 3, 4, 5)
+	doWorkWithSteward := newSteward(1*time.Millisecond, doWork)
+	doWorkWithSteward(done, 1*time.Hour)
+
+	for intVal := range take(done, intStream, 6) {
+		fmt.Printf("Received: %v\n", intVal)
+	}
+}
